b4a8e3caf69b5241fca4fc2389d88b71c00f3e6e
Beroris <legolizard157@gmail.com>
2025-03-13 00:04:30 +0000

    Added install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOTFILES_DIR=~/dotfiles
+
+ln -sf $DOTFILES_DIR/zshrc ~/.zshrc
+ln -sf $DOTFILES_DIR/gitconfig ~/.gitconfig
+ln -sf $DOTFILES_DIR/nvim-init.lua ~/.config/nvim/init.lua
+
+echo "Dotfiles have been symlinked!"
